12a716193c383842f17b71a9d3d0ed00b2ab3dc8
Atul Pandey <atul3pandey5@gmail.com>
2026-09-12 20:46:44 +0530

    Time: 2 ms (59.89%) | Memory: 55.3 MB (57.55%) - LeetSync

---
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+  const result = [];
+
+    for (const num of nums1) {
+        const index = nums2.indexOf(num);
+
+        if (index !== -1) {
+            result.push(num);
+            nums2.splice(index, 1);
+        }
+    }
+
+    return result;
+
+};
